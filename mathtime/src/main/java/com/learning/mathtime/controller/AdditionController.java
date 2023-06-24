32537076289c2c3f366f02f526ec0a163860c3e6
aaron (kb) <aaron.sanders342@gmail.com>
2023-06-24 17:09:06 -0500

    wip: exposed addition endpoint

--- mathtime/src/main/java/com/learning/mathtime/controller/AdditionController.java
+++ mathtime/src/main/java/com/learning/mathtime/controller/AdditionController.java
@@ -1,5 +1,16 @@
 package com.learning.mathtime.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AdditionController {
     
+    @GetMapping("/arithmetic/addition")
+    public ResponseEntity<String> getResource() {
+        // TODO: implement addition problem generation
+        String resource = "Time to add!";
+        return ResponseEntity.ok(resource);
+    }
 }
