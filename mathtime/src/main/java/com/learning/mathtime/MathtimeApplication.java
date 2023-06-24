ed1248820d89335d05e909adfd2e39fc8c1def71
aaron (kb) <aaron.sanders342@gmail.com>
2023-06-23 23:33:47 -0500

    create java backend spring project

---
+++ mathtime/src/main/java/com/learning/mathtime/MathtimeApplication.java
@@ -0,0 +1,13 @@
+package com.learning.mathtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathtimeApplication.class, args);
+	}
+
+}
---
+++ mathtime/src/test/java/com/learning/mathtime/MathtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.mathtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
