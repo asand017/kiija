6598ede28165486ab99586cf9db34080605fd46e
aaron (kb) <aaron.sanders342@gmail.com>
2023-06-27 22:17:24 -0500

    addition service junit

--- mathtime/src/test/java/com/learning/mathtime/service/AdditionServiceTest.java
+++ mathtime/src/test/java/com/learning/mathtime/service/AdditionServiceTest.java
@@ -1,5 +1,32 @@
 package com.learning.mathtime.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.learning.mathtime.common.AdditionSubtractionHelper;
+
+@ExtendWith(MockitoExtension.class)
 public class AdditionServiceTest {
+
+    @InjectMocks
+    private AdditionService additionService;
+
+    @Mock
+    private AdditionSubtractionHelper mockHelper;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void shouldReturnProblemSet() {
+        
+    }
     
 }
