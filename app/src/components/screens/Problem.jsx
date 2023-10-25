c78603ee73de8e5e6d239e39153b0d4af047de0e
aaron (kb) <aaron.sanders342@gmail.com>
2023-10-25 00:57:43 -0700

    wip: added problem form

--- app/src/components/screens/Problem.jsx
+++ app/src/components/screens/Problem.jsx
@@ -1,26 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import Grid from '@mui/material/Unstable_Grid2';
 import Page from "../common/Page";
+import { display } from "@mui/system";
+import { TextField } from "@mui/material";
 
 const Problem = (props) => {
 
     const navigate = useNavigate();
     const {state} = useLocation();
-    const problemSet = state.problems;
-    //const numOfOperands = problemSet.length();
+    const [problemSet, setProblemSet] = useState(state.problems);
+    const [currentProblem, setCurrentProblem] = useState(0);
     const [problemIndex, setProblemIndex] = useState(0);
+    const [problemList, setProblemList] = useState([]);
     const type = state.type;
 
     useEffect(() => {
-        console.log(problemSet[problemIndex].operands);
-        console.log(problemSet[problemIndex].solution);
+        //console.log(JSON.stringify(problemSet));
+        let newArray = problemSet.map((item, index) => ({index: index, operands: item.operands, solution: item.solution}));
+        console.log("new problem set: " + JSON.stringify(newArray));
+        setProblemSet(newArray);
     }, []);
+
+
+    useEffect(() => {
+        console.log("@@@" + problemSet);
+    }, [problemSet])
 
     return(
         <Page>
-            Problem set
-            {problemSet[problemIndex].operands.map((index, item) => {
-
+            <>Problem set</>
+            {problemSet.map((item) => {
+                console.log(item);
+                if(item.index === currentProblem){
+                    return (
+                        <Grid container key={item.index}>
+                            <Grid xs={12} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                {type}
+                            </Grid>
+                            {item.operands.map((operand) => {
+                                return(
+                                    <Grid xs={12}>{operand}</Grid>
+                                );
+                            })}
+                            <Grid>
+                                <TextField id="outlined-basic"
+                                    label="answer"
+                                    variant="outlined"
+                                    autoComplete="off"
+                                    type="text"
+                                />
+                            </Grid>
+                        </Grid>
+                    );
+                }
             })}
         </Page>
     );
