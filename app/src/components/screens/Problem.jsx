f9228cea5956376ea09a9296c4e125e24bbfd552
aaron (kb) <aaron.sanders342@gmail.com>
2023-11-15 16:07:43 -0600

    wip: problem set screen view sp

--- app/src/components/screens/Problem.jsx
+++ app/src/components/screens/Problem.jsx
@@ -3,14 +3,19 @@ import { useNavigate, useLocation } from "react-router-dom";
 import Grid from '@mui/material/Unstable_Grid2';
 import Page from "../common/Page";
 import { display } from "@mui/system";
-import { TextField } from "@mui/material";
+import { Button, Paper, TextField } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add'; // '+'
+import RemoveIcon from '@mui/icons-material/Remove'; // '-'
+import { styles } from "../common/Constants";
+import Add from "@mui/icons-material/Add";
 
 const Problem = (props) => {
 
     const navigate = useNavigate();
     const {state} = useLocation();
     const [problemSet, setProblemSet] = useState(state.problems);
-    const [currentProblem, setCurrentProblem] = useState(0);
+    const count = state.problems.length;
+    const [currentProblem, setCurrentProblem] = useState(1);
     const [problemIndex, setProblemIndex] = useState(0);
     const [problemList, setProblemList] = useState([]);
     const [ans, setAns] = useState('');
@@ -19,7 +24,7 @@ const Problem = (props) => {
     useEffect(() => {
         //console.log(JSON.stringify(problemSet));
         let newArray = problemSet.map((item, index) => ({index: index, operands: item.operands, solution: item.solution}));
-        console.log("new problem set: " + JSON.stringify(newArray));
+        console.log("new problem set: " + JSON.stringify(newArray) + ": " + count);
         setProblemSet(newArray);
     }, []);
 
@@ -30,39 +35,54 @@ const Problem = (props) => {
 
     return(
         <Page>
-            <>Problem set</>
-            {problemSet.map((item) => {
-                console.log(item);
-                if(item.index === currentProblem){
-                    return (
-                        <Grid container key={item.index}>
-                            <Grid xs={12} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                                {type}
+            <Paper variant="elevation" elevation={3}>
+                {problemSet.map((item) => {
+                    console.log(item);
+                    if(item.index === currentProblem){ // TODO: add view slider to display in horizontal or vertical
+                        return (
+                            <Grid container key={item.index} minHeight={320}>
+                                <Grid xs={12} sx={[styles.flex_centered]}>
+                                    <h2>{type} ({currentProblem} of {count})</h2>
+                                </Grid>
+                                <Grid container xs={12} sx={[styles.flex_centered]}>
+                                    {item.operands.map((operand, key) => {
+                                        const addSign = key > 0;
+                                        return(
+                                            <>
+                                                <Grid xs={6} display={'flex'} justifyContent={'right'} alignItems={'center'}>
+                                                    {addSign && <AddIcon/>}
+                                                </Grid>
+                                                <Grid key={key} xs={6} display={'flex'} justifyContent={'left'} alignItems={'center'} paddingLeft={3}>
+                                                    {operand}
+                                                </Grid>
+                                            </>
+                                        );
+                                    })}
+                                    <Grid xs={12} sx={[styles.flex_centered]}>
+                                        <div style={{width: "50%", height: "6px", backgroundColor: "black"}}/>
+                                    </Grid>
+                                </Grid>
+                                <Grid xs={6} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                                    <TextField 
+                                        id="outlined-basic"
+                                        label="answer"
+                                        variant="outlined"
+                                        type="text"
+                                        value={ans}
+                                        onFocus={(event) => {
+                                            if(event.target.autocomplete)
+                                                event.target.autocomplete = "new-password";
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid xs={6} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                                    <Button >turn in</Button>
+                                </Grid>
                             </Grid>
-                            {item.operands.map((operand, key) => {
-                                return(
-                                    <Grid key={key} xs={12}>{operand}</Grid>
-                                );
-                            })}
-                            <Grid>
-                                <TextField 
-                                    id="outlined-basic"
-                                    label="answer"
-                                    variant="outlined"
-                                    type="text"
-                                    value={ans}
-                                    onFocus={(event) => {
-                                        if(event.target.autocomplete)
-                                        {
-                                          event.target.autocomplete = "new-password";
-                                        } 
-                                    }}
-                                />
-                            </Grid>
-                        </Grid>
-                    );
-                }
-            })}
+                        );
+                    }
+                })}
+            </Paper>
         </Page>
     );
 }
